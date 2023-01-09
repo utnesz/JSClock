433fca6743202b12da60643a781b109613dbd49e
utnesz <90829509+utnesz@users.noreply.github.com>
2023-01-09 15:52:33 +0100

    toggle is ready

--- scripts.js
+++ scripts.js
@@ -4,3 +4,39 @@ const secondLmnt = document.querySelector('.second')
 const timeLmnt = document.querySelector('.time')
 const dateLmnt = document.querySelector('.date')
 const toggleLmnt = document.querySelector('.toggle')
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+toggleLmnt.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
