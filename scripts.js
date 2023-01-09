2e7a9876e07879b46d34c32d1941df8249be4a6f
utnesz <90829509+utnesz@users.noreply.github.com>
2023-01-09 16:13:08 +0100

    working on js logic

--- scripts.js
+++ scripts.js
@@ -40,3 +40,27 @@ toggleLmnt.addEventListener('click', (e) => {
         e.target.innerHTML = 'Light mode'
     }
 })
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    hourLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondLmnt.style.transform = `translate(-50%,-100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers (answer154)
+
+const scale = (num, inMin, inMax, outMin, outMax) => {
+    return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+setTime();
+
+setInterval(setTime, 1000)
